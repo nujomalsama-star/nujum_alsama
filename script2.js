bf6f2ef546bdbd9ad8ff879e4b31815d124ec6e3
nujomalsama-star <nujomalsama@gmail.com>
2025-11-12 16:19:12 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (navToggle && navMenu) {
         navToggle.addEventListener("click", () => {
-            // تبديل الكلاس 'active' لإظهار/إخفاء القائمة
+            
             navMenu.classList.toggle("active"); 
             
-            // (اختياري) تغيير أيقونة الزر من (bars) إلى (times)
+          
             const icon = navToggle.querySelector("i");
             if (navMenu.classList.contains("active")) {
                 icon.classList.remove("fa-bars");
@@ -19,29 +19,30 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-  // انتظر حتى يتم تحميل الصفحة بالكامل
+
     document.addEventListener("DOMContentLoaded", function() {
         
-        // ابحث عن كل الروابط في الصفحة التي تبدأ بعلامة #
+      
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             
-            // أضف "مستمع" لكل رابط لينتظر الضغط عليه
+          
             anchor.addEventListener('click', function(e) {
                 
-                // 1. أوقف السلوك الافتراضي (القفزة السريعة)
+               
                 e.preventDefault();
 
-                // 2. احصل على الـ id الخاص بالهدف (مثل #contact-us-section)
+              
                 let targetId = this.getAttribute('href');
                 let targetElement = document.querySelector(targetId);
 
-                // 3. قم بعمل سكرول ناعم إلى هذا الهدف
+               
                 if (targetElement) {
                     targetElement.scrollIntoView({
                         behavior: 'smooth',
-                        block: 'start' // اجعل بداية القسم هي بداية الشاشة
+                        block: 'start' 
                     });
                 }
             });
         });
+
     });
